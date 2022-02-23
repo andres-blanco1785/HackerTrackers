9449d4ae7a29acc123f9084eab6d6e0e99152d40
Lane Tapp <lane22@tamu.edu>
2022-02-23 13:32:49 -0600

    Testing getting final transaction

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, json, jsonify, request, Response
 from dotenv import load_dotenv
+from solana.rpc.api import Client
 import requests
 import os
 
@@ -21,6 +22,19 @@ def get_transaction_info(transactionID):
     response = requests.get(f'https://api.solanabeach.io/v1/transaction/{transactionID}', headers=request_headers)
     print('this is the response', response.json())
     return response.json()
+
+@app.route("/fin_transaction/<string:address>/<string:bef_addr>")
+def get_final_transaction(address, bef_addr):
+    http_client = Client("https://api.mainnet-beta.solana.com")
+    response = http_client.get_signatures_for_address(address, before=bef_addr)
+
+    if(len(response.get("result")) < 1000):
+        return response[-1]
+
+    else:
+        last = response[len(response) - 1]
+
+        return get_final_transaction(address, last.get)
 
 
 if __name__ == '__main__':
