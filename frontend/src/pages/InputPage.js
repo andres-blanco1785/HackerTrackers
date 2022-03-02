fd666ad305bee18811332b5da19ecb331c0183c7
matrev <markanthonytrevinojr@gmail.com>
2022-03-02 12:51:47 -0600

    styling shit

--- frontend/src/pages/InputPage.js
+++ frontend/src/pages/InputPage.js
@@ -8,24 +8,25 @@ export function InputPage() {
   const [transactionInput, setTransactionInput] = useState();
   const [transactionOutput, setTransactionOutput] = useState({});
 
-  // const fetchData = async () => {
-    
-  //   setTransactionOutput(response.data);
-  // };
-  
-  async function onStartTracingClick() {
+  const fetchData = async () => {
     const response = await axios.get(`http://127.0.0.1:5000/transaction/${transactionInput}`);
-    history.push({
-      pathname: "/#/output",
-      state: response.data
-    });
     setTransactionOutput(response.data);
-  }
+  };
+  
+  
+  // async function onStartTracingClick() {
+  //   const response = await axios.get(`http://127.0.0.1:5000/transaction/${transactionInput}`);
+  //   history.push({
+  //     pathname: "/#/output",
+  //     state: response.data
+  //   });
+  //   setTransactionOutput(response.data);
+  // }
 
   return (
     <div>
         <input type="text" onChange={(e) => setTransactionInput(e.target.value)} value={transactionInput}/>
-        <button onClick={onStartTracingClick}>Start Tracing!</button>
+        <button onClick={fetchData}>Start Tracing!</button>
         {transactionOutput.accounts &&
             <div>
             {transactionOutput.accounts.map((account, index ) => {
