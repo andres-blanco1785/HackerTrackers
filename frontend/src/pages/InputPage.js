53ae3ea4a1239bd3333bd66f8c0d6a85430c1e5a
matrev <markanthonytrevinojr@gmail.com>
2022-03-14 18:38:51 -0500

    basic navbar styling

--- frontend/src/pages/InputPage.js
+++ frontend/src/pages/InputPage.js
@@ -6,8 +6,6 @@ export function InputPage() {
   const [transactionInput, setTransactionInput] = useState();
   const navigate = useNavigate();
 
-  
-
   function onButtonClick() {
       navigate("/output", { state: transactionInput});
   }
