cf88842902c69de4500c1460df842526674353e4
Jonathan Torres <torres404@tamu.edu>
2022-03-18 10:17:03 -0500

    Completed basic function that returns suspicious accounts

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_scammer_address(transID):
     inputAccounts = curdict["inputAccount"]
     scammer_address = ""
     for i in inputAccounts:
-        if i["signer"] == False & i["writable"]:
+        if i["signer"] == False and i["writable"]:
             scammer_address = i["account"]
             break
     return scammer_address
@@ -59,13 +59,13 @@ def get_scammer_address(transID):
 ## if signer = false and writable = true in inputAccount
 def get_Data(transactionID):
     # get list of transactions after scam transaction
-    http_client = Client("https://ssc-dao.genesysgo.net/")
+    http_client = Client("https://bitter-floral-paper.solana-mainnet.quiknode.pro/dec0009263e0e71d4da5def5e085c744dce3d43a/")
     transaction_info = requests.get(f'https://public-api.solscan.io/transaction/{transactionID}')
     curdict = transaction_info.json()
     inputAccounts = curdict["inputAccount"]
     useraddress = ""
     for i in inputAccounts:
-        if i["signer"]  & i["writable"]:
+        if i["signer"]  and i["writable"]:
             useraddress = i["account"]
             break
     soltransfer = curdict["solTransfers"]
@@ -76,16 +76,16 @@ def get_Data(transactionID):
     curaddress = get_scammer_address(transactionID)
     trace_nodes += [(useraddress, curaddress, soltransferred)]
     suspicious_accounts+=[curaddress]
-    print("Initial trace nodes is: ", trace_nodes, end='\n')
-    print("Initial sus accs is: ", suspicious_accounts, end='\n')
-    response = http_client.get_signatures_for_address(curaddress, until=transactionID, limit=50)
+    #print("Initial trace nodes is: ", trace_nodes, end='\n')
+    #print("Initial sus accs is: ", suspicious_accounts, end='\n')
+    response = http_client.get_signatures_for_address(curaddress, until=transactionID)
     transactionlist = response.get("result")
     transactionlist.reverse()
-    for i in range(0, len(transactionlist)): # user recursive functions to body build array of suspicious accounts, and nodes
+    for i in range(31, len(transactionlist)): # user recursive functions to body build array of suspicious accounts, and nodes
         # error check transactions before adding more nodes
-        print("on iteration: ", i, end='\n')
+        #print("on iteration: ", i, end='\n')
         transaction = transactionlist[i]
-        time.sleep(2)
+        #time.sleep(2)
         response1 = http_client.get_transaction(transaction["signature"])
         # try:
         #     response1 = http_client.get_transaction(transaction["signature"])
@@ -105,54 +105,51 @@ def get_Data(transactionID):
         # else:
         #     response1 = http_client.get_transaction(transaction["signature"])
 
-
+        #print("Response was: ", response1)
         result = response1.get("result")
+        #print("Result was", result)
         meta = result.get("meta")
         postBalances = meta.get("postBalances")
         preBalances = meta.get("preBalances")
         transaction = result.get("transaction")
         message = transaction.get("message")
         accountKeys = message.get("accountKeys")
+        #print("len of account keys are: ", len(accountKeys))
+        curloc = 1
         if len(accountKeys) != 3:
             continue
         for i in range(0, len(accountKeys)):
-            if accountKeys[i]!="11111111111111111111111111111111" & accountKeys[i]!=useraddress:
-                otherloc = i
-            else:
+            if accountKeys[i]==curaddress:
                 curloc = i
-        if postBalances[curloc] >=preBalances[curloc]: #if current account did not send money
+                continue
+            elif accountKeys[i]=="11111111111111111111111111111111":
+                progloc = i
+                continue
+            else:
+                otherloc = i
+                continue
+        #print("Curloc, otherloc, progloc: ", curloc, otherloc, progloc)
+        #print("postbalance is", postBalances[curloc], "prebalance is", preBalances[curloc])
+        if postBalances[curloc] >= preBalances[curloc]: #if current account did not send money
             continue
 
 
-        cursolamount = postBalances[otherloc] - preBalances[otherloc]
+        cursolamount = (postBalances[otherloc] - preBalances[otherloc])* 0.000000001
 
         trace_nodes += [(curaddress, accountKeys[otherloc], cursolamount)]
         suspicious_accounts += [accountKeys[otherloc]]
 
-    ## use response.get("result") if response[result] doesnt work
-    print("final trace nodes is: ",trace_nodes, end='\n')
-    print("final sus accs is: ",suspicious_accounts, end='\n')
+    #print("final trace nodes is: ",trace_nodes, end='\n')
+    #print("final sus accs is: ",suspicious_accounts, end='\n')
     dictionary = {
-        "nodes":trace_nodes,
-        "transactions":suspicious_accounts
+        "transactions":trace_nodes,
+        "suspicious_accounts":suspicious_accounts
     }
 
     json_object = json.dumps(dictionary, indent=3)
-    print("The json is: ", json_object)
+    #print("The json is: ", json_object)
 
-def get_transaction_info(transactionID):
-    http_client = Client("https://ssc-dao.genesysgo.net/")
-    response = http_client.get_transaction(transactionID)
-    result = response.get("result")
-    meta = result.get("meta")
-    postBalances = meta.get("postBalances")
-    print(postBalances)
-    preBalances = meta.get("preBalances")
-    print(preBalances)
-    transaction = result.get("transaction")
-    message = transaction.get("message")
-    accountKeys = message.get("accountKeys")
-    print("len of account keys is: ", len(accountKeys))
+
 
 if __name__ == '__main__':
     app.run()
