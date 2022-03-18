9536de0aff9044d281777692b69b3d9206923190
Jonathan Torres <torres404@tamu.edu>
2022-03-18 18:08:44 -0500

    Refactored forward search

--- app.py
+++ app.py
@@ -33,121 +33,90 @@ def get_transaction_info(transactionID):
     return response.json()
 
 
-def get_scammer_address(transID):
+def get_initial_data(transID):
     transaction_info = requests.get(f'https://public-api.solscan.io/transaction/{transID}')
     curdict = transaction_info.json()
     inputAccounts = curdict["inputAccount"]
     scammer_address = ""
+    user_address = ""
+
     for i in inputAccounts:
         if i["signer"] == False and i["writable"]:
             scammer_address = i["account"]
-            break
-    return scammer_address
+        elif i["signer"]  and i["writable"]:
+            user_address = i["account"]
+    soltransfer = curdict["solTransfers"]
+    cursolamount = soltransfer[0]["amount"]
+    sol_exchanged = cursolamount * 0.000000001
+    return True, transID, user_address, scammer_address, sol_exchanged
 
-# def add_nodes(transID):
-#     curnodes = []
-#     http_client = Client("https://ssc-dao.genesysgo.net/")
-#     trace_nodes = []
-#     suspicious_nodes = []
-#     response = http_client.get_signatures_for_address(get_scammer_address(transactionID), until=transactionID)
-#     transactionlist = response["result"]
-#
-#
-#
-#     return curnodes
 
-## if signer = false and writable = true in inputAccount
+
+
+
+def get_transaction_data(transactionID, curaddress):
+    # IGNORE NON-SOL TRANSFERS
+    http_client = Client("https://bitter-floral-paper.solana-mainnet.quiknode.pro/dec0009263e0e71d4da5def5e085c744dce3d43a/")
+    response = http_client.get_transaction(transactionID)
+    result = response.get("result")
+    meta = result.get("meta")
+    postBalances = meta.get("postBalances")
+    preBalances = meta.get("preBalances")
+    transaction = result.get("transaction")
+    message = transaction.get("message")
+    accountKeys = message.get("accountKeys")
+    if len(accountKeys) != 3:
+        return False, 0, 0, 0, 0
+    for i in range(0, len(accountKeys)):
+        if accountKeys[i] == curaddress:
+            curloc = i
+            continue
+        elif accountKeys[i] == "11111111111111111111111111111111":
+            progloc = i
+            continue
+        else:
+            otherloc = i
+            continue
+    if postBalances[curloc] >= preBalances[curloc]:  # if current account did not send SOL
+        return False, 0, 0, 0, 0
+    # else, the current account sent SOL return data
+    cursolamount = (postBalances[otherloc] - preBalances[otherloc])* 0.000000001 # calculate SOL other account gained
+    return True, transactionID, curaddress, accountKeys[otherloc], cursolamount
+
+
 def get_Data(transactionID):
     # get list of transactions after scam transaction
     http_client = Client("https://bitter-floral-paper.solana-mainnet.quiknode.pro/dec0009263e0e71d4da5def5e085c744dce3d43a/")
-    transaction_info = requests.get(f'https://public-api.solscan.io/transaction/{transactionID}')
-    curdict = transaction_info.json()
-    inputAccounts = curdict["inputAccount"]
-    useraddress = ""
-    for i in inputAccounts:
-        if i["signer"]  and i["writable"]:
-            useraddress = i["account"]
-            break
-    soltransfer = curdict["solTransfers"]
-    cursolamount = soltransfer[0]["amount"]
-    soltransferred = cursolamount * 0.000000001
-    trace_nodes = []
-    suspicious_accounts = []
-    curaddress = get_scammer_address(transactionID)
-    trace_nodes += [(useraddress, curaddress, soltransferred)]
-    suspicious_accounts+=[curaddress]
-    #print("Initial trace nodes is: ", trace_nodes, end='\n')
-    #print("Initial sus accs is: ", suspicious_accounts, end='\n')
-    response = http_client.get_signatures_for_address(curaddress, until=transactionID)
+
+    initialdata = get_initial_data(transactionID)
+    transactions = []
+    accounts = []
+    scammer_address = initialdata[3]
+    transactions += [(initialdata[1], initialdata[2], initialdata[3], initialdata[4])]
+    accounts+=[scammer_address]
+    response = http_client.get_signatures_for_address(scammer_address, until=transactionID)
     transactionlist = response.get("result")
     transactionlist.reverse()
-    for i in range(31, len(transactionlist)): # user recursive functions to body build array of suspicious accounts, and nodes
+    for i in range(0, 128): 
         # error check transactions before adding more nodes
-        #print("on iteration: ", i, end='\n')
+
         transaction = transactionlist[i]
-        #time.sleep(2)
-        response1 = http_client.get_transaction(transaction["signature"])
-        # try:
-        #     response1 = http_client.get_transaction(transaction["signature"])
-        # except (RequestException, ReadTimeoutError, socket.timeout):
-        #     try:
-        #         http_client = Client("https://ssc-dao.genesysgo.net/")
-        #         print("error occured sleeping", end='\n')
-        #         time.sleep(10)
-        #         print("trying again", end='\n')
-        #         response1 = http_client.get_transaction(transaction["signature"])
-        #     except(SolanaRpcException):
-        #         http_client = Client("https://ssc-dao.genesysgo.net/")
-        #         print("error occured sleeping", end='\n')
-        #         time.sleep(10)
-        #         print("trying again", end='\n')
-        #         response1 = http_client.get_transaction(transaction["signature"])
-        # else:
-        #     response1 = http_client.get_transaction(transaction["signature"])
 
-        #print("Response was: ", response1)
-        result = response1.get("result")
-        #print("Result was", result)
-        meta = result.get("meta")
-        postBalances = meta.get("postBalances")
-        preBalances = meta.get("preBalances")
-        transaction = result.get("transaction")
-        message = transaction.get("message")
-        accountKeys = message.get("accountKeys")
-        #print("len of account keys are: ", len(accountKeys))
-        curloc = 1
-        if len(accountKeys) != 3:
+        result = get_transaction_data(transaction["signature"],scammer_address)
+        if result[0] == False:
             continue
-        for i in range(0, len(accountKeys)):
-            if accountKeys[i]==curaddress:
-                curloc = i
-                continue
-            elif accountKeys[i]=="11111111111111111111111111111111":
-                progloc = i
-                continue
-            else:
-                otherloc = i
-                continue
-        #print("Curloc, otherloc, progloc: ", curloc, otherloc, progloc)
-        #print("postbalance is", postBalances[curloc], "prebalance is", preBalances[curloc])
-        if postBalances[curloc] >= preBalances[curloc]: #if current account did not send money
-            continue
+        else:
+            transactions += [(result[1], result[2], result[3], result[4])]
+            accounts += [result[3]]
+        
 
-
-        cursolamount = (postBalances[otherloc] - preBalances[otherloc])* 0.000000001
-
-        trace_nodes += [(curaddress, accountKeys[otherloc], cursolamount)]
-        suspicious_accounts += [accountKeys[otherloc]]
-
-    #print("final trace nodes is: ",trace_nodes, end='\n')
-    #print("final sus accs is: ",suspicious_accounts, end='\n')
     dictionary = {
-        "transactions":trace_nodes,
-        "suspicious_accounts":suspicious_accounts
+        "transactions": transactions,
+        "accounts": accounts
     }
 
     json_object = json.dumps(dictionary, indent=3)
-    #print("The json is: ", json_object)
+
 
 
 
