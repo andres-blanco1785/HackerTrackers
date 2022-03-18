5c6598717b790c57137755b905880d5ab53403d8
Lane Tapp <lane22@tamu.edu>
2022-03-18 13:25:34 -0500

    Backwards trace working
    The backwards trace now works and returns a list of accounts and transactions

--- app.py
+++ app.py
@@ -26,27 +26,92 @@ def get_transaction_info(transactionID):
 
 @app.route("/fin_transaction/<string:transID>")
 def get_final_transaction(transID):
+    transactionID = transID
     http_client = Client("https://ssc-dao.genesysgo.net/")
-    response = http_client.get_transaction(transID)
+    response = http_client.get_transaction(transactionID)
     result = response['result']
-    generalBalChange = -1
+    accounts = []
+    transactions = []
+    transactions.append(transID)
+    preTokenArray = []
+    postTokenArray = []
+    genBalChange = -1
     tokenBalChange = -1
+    max = False
 
-    if(len(result['postBalances']) != 0):
-        generalBalChange = npy.max(npy.subtract(result['postBalances'], result['preBalances']))
-    if(len(result['postTokenBalances']) != 0):
-        tokenBalChange = npy.max(npy.subtract(result['postTokenBalances'], result['preTokenBalances']))
+    #determining which account gained money from the transaction
+    if(len(result['meta']['postBalances']) != 0):
+        genBalArray = npy.subtract(result['meta']['postBalances'], result['meta']['preBalances'])
+        genBalChange = npy.max(genBalArray)
+    if(len(result['meta']['postTokenBalances']) != 0):
+        for accountToken in result['meta']['postTokenBalances']:
+            postTokenArray.append(int(accountToken['uiTokenAmount']['amount']))
+        for preAccountToken in result['meta']['preTokenBalances']:
+            preTokenArray.append(int(preAccountToken['uiTokenAmount']['amount']))
+        tokenBalArray = npy.subtract(postTokenArray, preTokenArray)
+        tokenBalChange = npy.max(tokenBalArray)
+    if(tokenBalChange > genBalChange):
+        account_index = npy.where(tokenBalArray == tokenBalChange)[0][0]
+    else:
+        account_index = npy.where(genBalArray == genBalChange)[0][0]
 
-    #response = http_client.get_signatures_for_address(address, before=bef_addr)
-    list = response['result']
+    #gets the account id
+    account_list = result['transaction']['message']['accountKeys']
+    account = account_list[account_index]
+    accounts.append(account)
 
-    while(len(list) >= 1000):
-        bef_addr = list[0]['signature']
-        response = http_client.get_signatures_for_address(address, before=bef_addr)
+    while(len(accounts) <= 10 and max == False):
+
+        #start going backwards
+        response = http_client.get_signatures_for_address(account, before=transactionID)
         list = response['result']
-        #print("this is the list[0]: ", list[0])
+        if(len(list) < 1000):
+            transactionID = list[-1]['signature']
+            transactions.append(transactionID)
+        #get first transaction in an account
+        else:
+            num_iterations = 0
+            while(len(list) >= 1000 and num_iterations < 10):
+                bef_addr = list[0]['signature']
+                response = http_client.get_signatures_for_address(account, before=bef_addr)
+                list = response['result']
+                num_iterations += 1
+                transactionID = list[-1]['signature']
+                if(len(list) < 1000):
+                    transactions.append(transactionID)
+                if(num_iterations == 10):
+                    max = True
 
-    return list[0]
+        #if the last transaction can't be found, return what has been found
+        if(max == True):
+            return {"Transactions": transactions, "Accounts": accounts}
+
+        transactionID = transactions[-1]
+        response = http_client.get_transaction(transactionID)
+        result = response['result']
+
+        #determining which account lost money from the transaction
+        if(len(result['meta']['postBalances']) != 0):
+            genBalArray = npy.subtract(result['meta']['preBalances'], result['meta']['postBalances'])
+            genBalChange = npy.max(genBalArray)
+        if(len(result['meta']['postTokenBalances']) != 0):
+            for accountToken in result['meta']['postTokenBalances']:
+                postTokenArray.append(int(accountToken['uiTokenAmount']['amount']))
+            for preAccountToken in result['meta']['preTokenBalances']:
+                preTokenArray.append(int(preAccountToken['uiTokenAmount']['amount']))
+            tokenBalArray = npy.subtract(preTokenArray, postTokenArray)
+            tokenBalChange = npy.max(tokenBalArray)
+        if(tokenBalChange > genBalChange):
+            account_index = npy.where(tokenBalArray == tokenBalChange)[0][0]
+        else:
+            account_index = npy.where(genBalArray == genBalChange)[0][0]
+
+        #gets the account id
+        account_list = result['transaction']['message']['accountKeys']
+        account = account_list[account_index]
+        accounts.append(account)
+
+    return {"Transactions": transactions, "Accounts": accounts}
 
 
 if __name__ == '__main__':
