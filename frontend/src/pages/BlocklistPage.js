c3b8d68e56a716494978954057f35807ab47e958
matrev <markanthonytrevinojr@gmail.com>
2022-03-20 18:03:01 -0500

    blocklist page now works!

--- frontend/src/pages/BlocklistPage.js
+++ frontend/src/pages/BlocklistPage.js
@@ -1,31 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import Collapsible from '../components/Collapsible.js';
 import { getBlocklistedAccounts } from '../utilities/Utilities.js';
+import './BlocklistPage.css';
 
 export default function BlocklistPage(props) {
 
-    const [blocklistedAccounts, setBlocklistedAccounts] = useState({});
+    const [blocklistedAccounts, setBlocklistedAccounts] = useState([]);
+
+    async function fetchData() {
+        const accountsOBJ = await getBlocklistedAccounts();
+        const accountsJSON = JSON.parse(accountsOBJ);
+        setBlocklistedAccounts(accountsJSON.blacklistedAccounts);
+    }
 
     useEffect(async () => {
-        const accounts = await getBlocklistedAccounts();
-        setBlocklistedAccounts(JSON.parse(accounts));
+        fetchData();
     }, []);
-
-    useEffect(() => {
-        console.log("blacklistedAccounts: ", blocklistedAccounts.blocklistedAccounts);
-    }, [blocklistedAccounts]);
 
     return (
         <div>
-            this is a test
-            {/* {(typeof blocklistedAccounts !== 'undefined') && 
-            (
-                blocklistedAccounts.blocklistedAccounts.map(({ account, transactions}) => {
-                    <div>
-                        <p key={account}>{account} : {transactions} </p>
+            <h1>Blocklisted Accounts:</h1>
+            {blocklistedAccounts.map((obj, i) => {
+                return (
+                    <div key={i}>
+                        <Collapsible label={<a href={`https://explorer.solana.com/address/${obj.account}`} target="_blank" >{obj.account}</a>}>
+							<h3> Transactions:</h3>
+                            <a href={`https://explorer.solana.com/tx/${obj.transactions}`} target="_blank">{obj.transactions}</a>
+                            
+						</Collapsible>
                     </div>
-                })
-            )
-            } */}
+                )
+            })}
         </div>
     )
 }
