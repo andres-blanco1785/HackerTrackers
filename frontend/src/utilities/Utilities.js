ef148d7106349963abc7f35c2552dc4182a4fe92
matrev <markanthonytrevinojr@gmail.com>
2022-03-20 09:27:00 -0500

    added connection to frontend and backend

--- frontend/src/utilities/Utilities.js
+++ frontend/src/utilities/Utilities.js
@@ -12,9 +12,14 @@ export async function getBackwardsTrace(n) {
     let trace = await fetch(`${backendURL}/fin_transaction/${n}`)
         .then(response => response.text())
         .then(data => { return data });
+    
     return trace
 }
 
-// export async function getBlocklistedAccounts() {
-//     return 
-// }
+export async function getBlacklistedAccounts() {
+    let accounts = await fetch(`${backendURL}/get-blacklisted-accounts`)
+        .then(response => response.text())
+        .then(data => { return data });
+    
+    return accounts; 
+}
