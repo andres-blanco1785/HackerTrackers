d7d2bcbc5b9c2514faa4fef736a3a17dae79f96d
matrev <markanthonytrevinojr@gmail.com>
2022-03-20 19:01:04 -0500

    output page now shows backwards trace

--- frontend/src/pages/BlocklistPage.js
+++ frontend/src/pages/BlocklistPage.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'reactstrap';
+import { useNavigate } from 'react-router';
 import Collapsible from '../components/Collapsible.js';
 import { getBlocklistedAccounts } from '../utilities/Utilities.js';
 import './BlocklistPage.css';
@@ -6,6 +8,7 @@ import './BlocklistPage.css';
 export default function BlocklistPage(props) {
 
     const [blocklistedAccounts, setBlocklistedAccounts] = useState([]);
+    const navigate = useNavigate();
 
     async function fetchData() {
         const accountsOBJ = await getBlocklistedAccounts();
@@ -19,18 +22,28 @@ export default function BlocklistPage(props) {
 
     return (
         <div>
-            <h1>Blocklisted Accounts:</h1>
-            {blocklistedAccounts.map((obj, i) => {
-                return (
-                    <div key={i}>
-                        <Collapsible label={<a href={`https://explorer.solana.com/address/${obj.account}`} target="_blank" >{obj.account}</a>}>
-							<h3> Transactions:</h3>
-                            <a href={`https://explorer.solana.com/tx/${obj.transactions}`} target="_blank">{obj.transactions}</a>
-                            
-						</Collapsible>
-                    </div>
-                )
-            })}
+            {(blocklistedAccounts.length === 0) ? 
+                <div>
+                    <h1>There are no blocklisted accounts currently!</h1>
+                    <h2>Help report accounts by tracing fraudulent transactions</h2>
+                    <Button onClick={() => {navigate("/input", { state: {}})}}>Go Trace!</Button>
+                </div>
+            :
+                <div>
+                    <h1>Blocklisted Accounts:</h1>
+                    {blocklistedAccounts.map((obj, i) => {
+                        return (
+                            <div key={i}>
+                                <Collapsible label={<a href={`https://explorer.solana.com/address/${obj.account}`} target="_blank" >{obj.account}</a>}>
+                                    <h3> Transactions:</h3>
+                                    <a href={`https://explorer.solana.com/tx/${obj.transactions}`} target="_blank">{obj.transactions}</a>
+                                    
+                                </Collapsible>
+                            </div>
+                        )
+                    })}
+                </div>
+            }
         </div>
     )
 }
