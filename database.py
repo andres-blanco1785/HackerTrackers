dadac6a849b8c885683b6feb46c669950c8cd0d7
Lane Tapp <lane22@tamu.edu>
2022-03-27 18:28:05 -0500

    refactored database

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ import json
 import time
 
 #This methods takes dictionaries from the forward and bakcwards trace and populates the database
-def populate_data(txn, wallet):
+def populate_data(txn, wallet, blacklist_flag):
     con = psycopg2.connect(
         host="localhost",
         database="badActors",
@@ -18,7 +18,12 @@ def populate_data(txn, wallet):
         password="postgres"
     )
     cur = con.cursor()
-    cur.execute("insert into blacklist (transaction_id, accountwallet) values (%s,%s);", (txn,wallet))
+    psycopg2.extras.register_uuid()
+    if blacklist_flag:
+        cur.execute("insert into transactions (id, transaction_id, accountwallet) values (%s,%s,%s);", (uuid.uuid4(),txn,wallet))
+        cur.execute("insert into blacklist (accountwallet, is_blacklisted) values (%s, %s);", (wallet, "true"))
+    else:
+        cur.execute("insert into transactions (id, transaction_id, accountwallet) values (%s,%s,%s);", (uuid.uuid4(),txn,wallet))
     con.commit()
     con.close()
     return
@@ -31,16 +36,17 @@ def show_blacklist():
         password="postgres"
     )
     cur = con.cursor()
-    cur.execute("select * from blacklist")
+    cur.execute("select accountwallet from blacklist;")
     con.commit()
     blacklist = cur.fetchall()
-    con.close()
-
     blacklistJSON = []
     for account in blacklist:
+        cur.execute("select transaction_id from transactions where accountwallet = (%s);", (account))
+        con.commit()
+        txns = cur.fetchall()
         blacklistJSON.append({
-            "account": account[1],
-            "transactions": account[0]
+            "account": account,
+            "transactions": txns
         })
-
+    con.close()
     return jsonify({"blacklistedAccounts" : blacklistJSON})
